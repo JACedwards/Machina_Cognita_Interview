f670537164991b74914cc4334828de6f9018e03d
JACedwards <98490993+JACedwards@users.noreply.github.com>
2022-11-17 11:53:07 -0500

    Prepare_multi-parts_of_speech_for_prentation

--- Presentation_Versions/Multi_Parts_o_Speech.py
+++ Presentation_Versions/Multi_Parts_o_Speech.py
@@ -9,7 +9,6 @@ def parse(s):
     verb_lst = verb()
     art_lst = art()
 
-    # Question mark check:
     if '?' in s:
         return 'Question: Ends in question mark'
 
@@ -19,54 +18,56 @@ def parse(s):
    
     for w in s_lst:
         if w in let_num:
-            sent_d[w] = master_d.get(w)  #deals with letter-number hybrid (3dc)
+            sent_d[w] = master_d.get(w)  
         else: 
             sent_d[w] = master_d.get(w.lower())
     p_o_s = list(sent_d.values())
 
-    #Deals with any instance where same word could be verb or noun
+
+
+    ###Deals with any instance where same word could be verb or noun###
+
     for i in range(len(s_lst)):
         if s_lst[i].lower() in noun_lst and s_lst[i].lower() in verb_lst:
             if i == 0:
                 p_o_s[0] = 'verb'
-                print(f"In this sentence: \n\t{s} \n\t 'request' = verb")
+                print(f"\nSentence: \n\t{s} \n\n Part of speech:  \n\t'request' = verb")
             else:
                 if s_lst[i-1] in art_lst:
                     p_o_s[i] = 'noun'
                     sent_d[s_lst[i]] = 'noun'
-                    print(f"In this sentence: \n\t{s} \n\t 'request' = noun")
+                    print(f"\nSentence: \n\t{s} \n\n Part of speech:  \n\t'request' = noun")
 
                 else:
                     p_o_s[i] = 'verb'
                     sent_d[s_lst[i]] = 'verb'
-                    print(f"In this sentence: \n\t{s} \n\t 'request' = verb")
+                    print(f"\nSentence: \n\t{s} \n\n Part of speech:  \n\t'request' = verb")
+
+    ###End of multi-POS check###
 
 
-    #Check part of speech for 1st word
-
-    # Conditional verb = 1st:
     if p_o_s[0] == 'conditional':
-        return f'\nCategory = Question (conditional)'
+        return f'\nCategory: \n\tQuestion (conditional)\n\n-------------'
 
     # Interogative = 1st:
     if p_o_s[0] == 'interogative':
-        return f'\nCategory = Question (interrogative)'
+        return f'\nCategory: \n\tQuestion (interrogative)\n\n-------------'
 
     # Other verb = 1st:
     if p_o_s[0] == 'verb' and s_lst[0][-2:] != 'ed' and s_lst[0][-3:] != 'ing' and s_lst[0][-1:] != 's':
         if len(s_lst) <= 2:
-            return f'\nCategory = Command (2 or fewer words)'
+            return f'\nCategory: \n\tCommand (2 or fewer words)\n\n-------------'
         if p_o_s[1] == 'noun' or p_o_s[1] == 'pronoun':
-            return f'\nCategory = Question (verb followed by pro/noun)'
+            return f'\nCategory: \n\tQuestion (verb followed by pro/noun)\n\n-------------'
         else:
-            return f'\nCategory = Command'
+            return f'\nCategory: \n\tCommand\n\n-------------'
     
     #Something else = 1st:
-    return f'\nCategory = Observation'
+    return f'\nCategory: \n\tObservation\n\n-------------'
 
 print(parse('Can you make a request tomorrow'))  
-# print(parse('Request for cougar to fly'))
-# print(parse('Tomorrow I will request a boat'))
+print(parse('Request for cougar to fly'))
+print(parse('Tomorrow I will request a boat'))
 
 
 
